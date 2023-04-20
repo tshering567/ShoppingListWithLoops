5ef0506d4c0378669ccb542d63cace0c747ec3e3
Paul Kimball <pekimball@interfaceassociates.net>
2023-04-20 12:03:24 -0400

    Updated code

--- src/main/java/org/yearup/shopping/ShoppingListWithLoopsMain.java
+++ src/main/java/org/yearup/shopping/ShoppingListWithLoopsMain.java
@@ -3,7 +3,21 @@ package org.yearup.shopping;
 public class ShoppingListWithLoopsMain {
     public static void main(String[] args) {
 
-        Item item = new Item("F-1123", "Delicious Baklava 12 oz. bonus pack", 23.00, true);
-        System.out.println(item);
+
+        // Show the user a list of items for sale
+
+        Item[] itemsForSale = ItemsForSale.itemsForSale;
+        for (Item i : itemsForSale){
+            System.out.println(i);
+
+        }
+
+        // Invite the user to buy something
+
+        // Read in the user's response
+
+        // Parse their choice
+
+        // Tell the user they can have it
     }
 }
